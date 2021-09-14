b36a62fcb5e8f6159a41c11ff68f0a891151106a
fuffy <83110262+fuffy35@users.noreply.github.com>
2021-09-13 18:32:34 -0700

    store.js - add tests for playbacks

--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -23,44 +23,132 @@ describe('HelloWorld.vue', () => {
 jest.mock('axios');
 
 describe('store.js', () => {
-	const musicHistory = {
-		aStartDate: '2021-09-10 00:00',
-		aEndDate: '2021-09-11 21:00',
-		playbacks: [
-			{
-				album: 'Test Album',
-				albumArtist: 'Test Artist',
-				artist: 'Test Artist',
-				computer: 'Home',
-				dbCreated: '',
-				dbModified: null,
-				filepathRaw: '',
-				filesize: 45616820,
-				filesizeNatural: '43.5 MB',
-				foobar2000version: 'foobar2000 v1.3.10',
-				lengthEx: '30:00.072',
-				lengthSeconds: 1800,
-				songStarted: '2021-09-11 15:34:09',
-				tempID: 42,
-				title: 'Song Title',
-				trackArtist: null,
-				trackNumber: null
-			}
-		]
-	};
+	let musicHistory;
+	let musicHistoryString;
 
-	test('store.state', () => {
-		console.log(state);
+	beforeAll(() => {
+		musicHistory = {
+			aStartDate: '2021-09-10 00:00',
+			aEndDate: '2021-09-11 21:00',
+			playbacks: [
+				{
+					album: 'Test Album',
+					albumArtist: 'Test Artist',
+					artist: 'Test Artist',
+					computer: 'Home',
+					dbCreated: '',
+					dbModified: null,
+					filepathRaw: '',
+					filesize: 45616820,
+					filesizeNatural: '43.5 MB',
+					foobar2000version: 'foobar2000 v1.3.10',
+					lengthEx: '30:00.072',
+					lengthSeconds: 1800,
+					songStarted: '2021-09-11 15:34:09',
+					tempID: 42,
+					title: 'Song Title',
+					trackArtist: null,
+					trackNumber: null
+				}
+			]
+		};
+
+		musicHistoryString = JSON.stringify(musicHistory)
+			.replace(/"/g, '&#34;')
+			.replace(/'/g, '&#39;');
 	});
-	test('store.getters', () => {
-		console.log(getters);
+
+	/* --------------------------------------------------------------------------------------------------------- */
+
+	describe('getters', () => {
+		test('playbacks', () => {
+			const state = { playbacks: musicHistory.playbacks };
+			expect(getters.playbacks(state)).toEqual(musicHistory.playbacks);
+		});
 	});
-	test('store.actions', () => {
-		console.log(actions);
+
+	/* --------------------------------------------------------------------------------------------------------- */
+
+	describe('actions', () => {
+
+		/* ------------------------------------------ */
+
+		test('getPlaybacks', async() => {
+			axios.get.mockResolvedValue({data: musicHistoryString});
+
+			const commit = jest.fn();
+
+			await actions.getPlaybacks({ commit });
+
+			/*
+			console.log(commit.mock);
+				console.log tests/unit/example.spec.js:137
+				  { calls: [ [ 'SET_PLAYBACKS', [Array] ] ],
+				    instances: [ undefined ],
+				    invocationCallOrder: [ 2 ],
+				    results: [ { type: 'return', value: undefined } ] }
+			*/
+			expect(commit.mock.calls).toEqual([
+				['SET_PLAYBACKS', musicHistory.playbacks]
+			]);
+		});
+
+		/* ------------------------------------------ */
+			// yoink: https://vuex.vuejs.org/guide/testing.html
+			// (this is how you'd do the above if you don't have jest.fn() or similar.)
+			// (but since we do have that, we can write it in a much shorter way.)
+		/* ------------------------------------------ */
+		/*
+		// helper for testing action with expected mutations
+		const testAction = (action, payload, state, expectedMutations, done) => {
+			let count = 0;
+
+			// mock commit
+			const commit = (mutationType, payload) => {
+				const mutation = expectedMutations[count];
+
+				try {
+					expect(mutationType).toEqual(mutation.type);
+					expect(payload).toEqual(mutation.payload);
+				} catch (error) {
+					done(error);
+				}
+
+				count++;
+
+				if (count >= expectedMutations.length) {
+					done();
+				}
+			}
+
+			// call the action with mocked store and arguments
+			action({ commit, state }, payload);
+
+			// check if no mutations should have been dispatched
+			if (expectedMutations.length === 0) {
+				expect(count).toBe(0);
+				done();
+			}
+		}
+
+		test('getPlaybacks', done => {
+			axios.get.mockResolvedValue({data: musicHistoryString});
+
+			// const testAction = (action, payload, state, expectedMutations, done) => {...}
+			testAction(
+				actions.getPlaybacks,
+				null,
+				{ playbacks: [] },
+				[
+					{ type: 'SET_PLAYBACKS', payload: musicHistory.playbacks }
+				],
+				done
+			);
+		});
+		*/
 	});
-	test('store.mutations', () => {
-		console.log(mutations);
-	});
+
+	/* --------------------------------------------------------------------------------------------------------- */
 
 	describe('mutations', () => {
 		test('SET_PLAYBACKS', () => {
@@ -68,14 +156,6 @@ describe('store.js', () => {
 			mutations.SET_PLAYBACKS(state, musicHistory.playbacks);
 			expect(state.playbacks).toEqual(musicHistory.playbacks);
 		});
-	});
-
-	test('should fetch music history', () => {
-		axios.get.mockResolvedValue({data: musicHistory});
-
-		//console.log(mutations);
-		//store.actions.getPlaybacks();
-		//console.log(getters.playbacks);
 	});
 });
 
